5847b1eb3e08134b71b48ee00b05d8316bb21044
abdulloh75 <abdulloxqayumov448@gmail.com>
2023-12-29 18:33:47 +0500

    complete

--- src/client/components/Navbar/Navbar.jsx
+++ src/client/components/Navbar/Navbar.jsx
@@ -43,11 +43,11 @@ const Navbar = () => {
           }
         </div>
         {/* linke items */}
-        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-15' : 'top-[-490px]'}`}>
+        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-15' : 'top-[-490px]'}`}>
           {
             Links.map((link) => (
               <li className='md:ml-8 md:my-0 my-7 font-semibold'>
-                <a href={link.link} className='text-gray-800 hover:text-blue-400 duration-500'>{link.name}</a>
+                <a href={link.link} className='text-white hover:text-blue-400 duration-500'>{link.name}</a>
               </li>))
           }
           <div className="gap-5 flex">
