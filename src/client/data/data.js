2276f4dbcf4b2c3dd422b35abbd37877ae6a5e59
abdulloh75 <abdulloxqayumov448@gmail.com>
2023-12-30 14:55:26 +0500

    push

--- src/client/data/data.js
+++ src/client/data/data.js
@@ -44,5 +44,110 @@ export const mockData = {
       series: "6 SERIES",
       video: '15 VIDEOS'
     },
+    {
+      id: 7,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
+    {
+      id: 8,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
+    {
+      id: 9,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
+    {
+      id: 10,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
+    {
+      id: 11,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
+    {
+      id: 12,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
+    {
+      id: 13,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
+    {
+      id: 14,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
+    {
+      id: 15,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
+    {
+      id: 16,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
+    {
+      id: 17,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
+    {
+      id: 18,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
+    {
+      id: 19,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
+    {
+      id: 20,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
+    {
+      id: 21,
+      image: logo,
+      title: 'REACTJS',
+      series: "4 SERIES",
+      video: '14 VIDEOS'
+    },
   ]
 };
